123166b5bf1fbc04b494cde8852b1aaae61c7d39
Rudresh Gowda <rudresh9nr@gmail.com>
2026-06-12 16:28:17 +0530

    Add order creation logging

--- backend/orders/api.py
+++ backend/orders/api.py
@@ -4,9 +4,12 @@ from django.http import JsonResponse
 from django.views import View
 from orders.models import Order
 from outbox.models import OutboxEvent
-
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 # Need to remove this in production
 @method_decorator(csrf_exempt, name="dispatch")
@@ -20,7 +23,17 @@ class OrderCreateView(View):
             total_amount = data["total_amount"],
             currency = data.get("currency", "INR"),
         )
-    
+
+        logger.info(
+            "Order Created",
+            extra={
+                "order_id": str(order.id),
+                "user_id": str(order.user_id),
+                "amount": float(order.total_amount),
+                "currency": order.currency,
+            },
+        )
+
         OutboxEvent.objects.create(
             aggregate_type = "Order",
             aggregate_id = order.id,
